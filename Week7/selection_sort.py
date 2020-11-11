7317bc52efa167bc3bd0003a658ef5aaa7a097d0
repl.it user <replituser@example.com>
2020-11-11 16:32:41 +0000

    Added code to demonstrate insertion sorting.

--- Week7/selection_sort.py
+++ Week7/selection_sort.py
@@ -1,9 +1,9 @@
-numbers = [64, 25, 12, 22, 11]
+numbers = [64, 25, 12, 22, 11, 65]
 
-for i in range(len(numbers)):
+for i in range(len(numbers)- 1):
   lowest_num = i
-  for j in range(i + 1, len(numbers)):
-    if numbers[lowest_num] > numbers[j]:
+  for j in range(i+1, len(numbers)- 1):
+    if numbers[j] < numbers[lowest_num]:
       lowest_num = j
   
   numbers[i], numbers[lowest_num] = numbers[lowest_num], numbers[i]
